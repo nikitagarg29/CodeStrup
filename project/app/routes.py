22342ba3c276fe90fec12c58dbc211f6853cecd0
Shruthi Rao <raoshruthi2001@gmail.com>
2020-12-19 18:49:09 +0530

    added quiz1 column in db and made quiz ll

--- project/app/routes.py
+++ project/app/routes.py
@@ -12,15 +12,6 @@ import sqlite3
 def index():
     return render_template('index.html', title = 'Code Guide')
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         flash('Login requested for user {}, remember_me={}'.format(
-#             form.username.data, form.remember_me.data))
-#         return redirect('/index')
-#     return render_template('login.html', title='Sign In', form=form)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -74,6 +65,40 @@ def queue():
 def linkedlist():
     return render_template('linkedlist.html', title = 'Code Guide')
 
+questions = [
+    {
+        "id": "1",
+        "question": "What is the Capital of Syria?",
+        "answers": ["a) Beirut", "b) Damascus", "c) Baghdad"],
+        "correct": "b) Damascus"
+    },
+    {
+        "id": "2",
+        "question": "What is the square root of Pi?",
+        "answers": ["a) 1.7724", "b) 1.6487", "c) 1.7872"],
+        "correct": "a) 1.7724"
+    },
+    {
+        "id": "3",
+        "question": "How many counties are there in England?",
+        "answers": ["a) 52", "b) 48", "c) 45"],
+        "correct": "b) 48"
+    }
+]
+
+@app.route('/quiz1')
+def quizll():
+    if request.method == 'GET':
+        return render_template("quizll.html", data=questions)
+    else:
+        result = 0
+        total = 0
+        for question in questions:
+            if request.form[question.get('id')] == question.get('correct'):
+                result += 1
+            total += 1
+        return render_template('results.html', total=total, result=result)
+    #return render_template('quizll.html', title = 'Linked List Quiz')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
